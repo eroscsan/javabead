f910dd02e0cd9cf94a6fbf7197a22a1623098b8c
Unknown <eroscsan@gmail.com>
2017-12-14 10:54:27 +0100

    javabead_v1

---
+++ payment-dao/src/main/java/hu/ppke/itk/hu/erocs/payment_dao/payment_dao/App.java
@@ -0,0 +1,13 @@
+package hu.ppke.itk.hu.erocs.payment_dao.payment_dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
